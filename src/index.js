fb146133d7a9776c688751efde5e6f98eb5a6c9c
myqkq111 <myqkq111@naver.com>
2024-06-27 19:06:30 +0900

    필터 RU 구현 + 미완료 일정 자동 등록 구현

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import passport from "passport";
-import usersRouter from "./routes/usersRouter.js";
+import filterRouter from "./routes/filterRouter.js";
+import cors from "cors";
+import Schedule from "./middleware/Schedule.js";
 
 const app = express();
 connectDB();
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(passport.initialize());
 
-app.use("/api/users", usersRouter);
+Schedule();
+
+app.use("/api/filter", filterRouter);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
